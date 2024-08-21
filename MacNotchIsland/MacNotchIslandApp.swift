13adf1fc0a80de28d1925a37291283d31ea3a5b8
Shiyam Adhithya V P <shiyamhhh@gmail.com>
2024-08-21 22:29:43 +0530

    Initial Commit

---
+++ MacNotchIsland/MacNotchIslandApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacNotchIslandApp.swift
+//  MacNotchIsland
+//
+//  Created by Shiyam   on 8/21/24.
+//
+
+import SwiftUI
+
+@main
+struct MacNotchIslandApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
